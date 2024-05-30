fd1b83ab50a37ce9a2fc7a4d90fb4d85915eb9b5
sanchit-4 <145276504+sanchit-4@users.noreply.github.com>
2024-05-30 23:59:16 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import lighthouse from "@lighthouse-web3/sdk";
-import "./App.css"; // Import CSS file for styling
+import "./App.css"; // Import the CSS file
 
 function App() {
   const progressCallback = (progressData) => {
@@ -9,31 +9,52 @@ function App() {
     console.log(percentageDone);
   };
 
-  const uploadFile = async (file) => {
+  const uploadFile = async (file, type) => {
     const output = await lighthouse.upload(
       file,
-      "<API_KEY>",
+      "e0de6603.9d51a215958d42249f66049a26a15fad",
       false,
       null,
       progressCallback
     );
     console.log("File Status:", output);
-
     console.log(
-      "Visit at https://gateway.lighthouse.storage/ipfs/" + output.data.Hash
+      `Visit at https://gateway.lighthouse.storage/ipfs/${output.data.Hash}`
     );
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Upload your Advertisement Here</h1>
-        <input
-          className="File-input"
-          onChange={(e) => uploadFile(e.target.files)}
-          type="file"
-        />
+        <img src="/company_logo.png" alt="Company Logo" className="logo" />
+        <h1>Ad Upload Dashboard</h1>
       </header>
+      <div className="upload-section">
+        <div className="upload-window">
+          <h2>Upload Poster</h2>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => uploadFile(e.target.files, "photo")}
+          />
+        </div>
+        <div className="upload-window">
+          <h2>Upload Video</h2>
+          <input
+            type="file"
+            accept="video/*"
+            onChange={(e) => uploadFile(e.target.files, "video")}
+          />
+        </div>
+        <div className="upload-window">
+          <h2>Upload Ad description</h2>
+          <input
+            type="file"
+            accept=".txt"
+            onChange={(e) => uploadFile(e.target.files, "text")}
+          />
+        </div>
+      </div>
     </div>
   );
 }
